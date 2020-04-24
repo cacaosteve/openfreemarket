3c88b39e24a90911af9b954acfa276750062af04
Stephen Zyszkiewicz <stevez@me.com>
2020-04-24 11:46:58 -0500

    fix languages

--- db/migrate/20170414144102_rename_languanges_table.rb
+++
@@ -1,9 +0,0 @@
-class RenameLanguagesTable < ActiveRecord::Migration[5.0]
-  def self.up
-    rename_table :languages, :languages
-  end
-
-  def self.down
-    rename_table :languages, :languages
-  end
-end
