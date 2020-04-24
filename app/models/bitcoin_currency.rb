77520b5227257ed7b9733bff7d57e5095c00b10a
Stephen Zyszkiewicz <stevez@me.com>
2020-04-24 11:37:15 -0500

    fix URI.open

--- app/models/bitcoin_currency.rb
+++ app/models/bitcoin_currency.rb
@@ -1,6 +1,6 @@
 class BitcoinCurrency < ActiveRecord::Base
 	def self.reset_currencies
-    currencies = JSON.load(open("https://bitpay.com/api/rates/BCH"))
+    currencies = JSON.load(URI.open("https://bitpay.com/api/rates/BCH"))
     # bch = currencies.select { |currency| currency["code"] == "BCH" }.first
     # currencies.each { |r| r["rate"] = r["rate"] / bch["rate"] }
     currencies.each { |r| r["rate"] = r["rate"] }
